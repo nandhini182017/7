e94198c4d5957cb83fe5118178626d5671e285bc
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-07 13:55:47 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+printf("Enter two numbers(intervals): ");
+  scanf("%d %d", &n1, &n2);
+  printf("Armstrong numbers between %d an %d are: ", n1, n2);
+  for(i=n1+1; i<n2; ++i)
+  {
+      temp=i;
+      num=0;
+      while(temp!=0)
+      {
+          rem=(temp%10);
+          num+=rem*rem*rem;
+          temp/=10;
+      }
+    }
+    }
+  
