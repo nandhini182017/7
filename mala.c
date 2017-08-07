d1799b84239258162d02677ff9e5b85027eb30f0
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-07 14:15:45 +0530

    Create mala.c

---
+++ mala.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    char d;
+    printf("Enter a character: ");
+    scanf("%d",&d);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%d is an alphabet.",d);
+    else
+        printf("%d is not an alphabet.",d);
+        }
