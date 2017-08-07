e5c4e5047c7db40c16a3a0e4e29aceb92554f82d
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-07 13:59:53 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int n, i;
+   printf("Enter an integer: ");
+    scanf("%d",&n);
+    printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+}
+    
