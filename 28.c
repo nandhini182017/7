31fc60be08847c86d5c8a439ece6584bfc4ce78f
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-08 11:58:06 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+printf("Print 1 to 5 again and again");
+while(1){
+for(i=1;i<=5;i++)
+printf("\n%d",i);
+ ch=getch();
+  if(ch=='Q')
+  exit(0);
+ }
+ getch();
+ return0;
+    }
