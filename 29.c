5e62714c1234a8b6dceea79f8298301abad17f6f
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-08 12:02:42 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+  int n, i, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    i = 1;
+    while ( i <=n )
+    {
+     sum += i;
+     ++i;
+    }
+    printf("Sum = %d",sum);
+    }
