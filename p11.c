1377a44652969f18ef856ecbb1f42ec3b844f79f
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-10 04:42:20 -0700

    Rename p1.c to p11.c

---
+++ p11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char str[90];
+    char *rev;
+    printf("Enter any string : ");
+    scanf("%d",str);
+    rev = strrev(str);
+  printf("Reverse string is : %d",rev);
+ }
