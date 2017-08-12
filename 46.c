a2794ae921e6e0bc44eb357d2032819e57ecd108
nandhini182017 <30342566+nandhini182017@users.noreply.github.com>
2017-08-12 04:24:18 -0700

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+  int n, i;
+  printf("Enter an integer: ");
+    scanf("%s",&n);
+
+    for(i=1; i<=10; ++i)
+    {
+        printf("%s * %s = %s \n", n, i, n*i);
+    }
+    }
+    
+    
